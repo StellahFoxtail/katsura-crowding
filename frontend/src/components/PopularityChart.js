c0bf3e54cfd24f331dc9d49e8c6ea91e903c7ab7
Alvin B. Kimbowa <kimbowa2alvin@gmail.com>
2023-01-07 14:09:10 +0300

    Updated the url to the api

--- frontend/src/components/PopularityChart.js
+++ frontend/src/components/PopularityChart.js
@@ -24,10 +24,11 @@ const PopularityChart = ({ location }) => {
                                         "time_to_display": null,
                                       });
   
+  const apiURL = "https://katsuracrowding.pythonanywhere.com";
 
   useEffect(() => {
     console.log("Fetching data...")
-    fetch(`http://127.0.0.1:5000/day_average/${location}`)
+    fetch(`${apiURL}/day_average/${location}`)
      .then(res => res.json())
      .then(json => setData(json))
      .catch(error => console.error(error))
@@ -35,13 +36,13 @@ const PopularityChart = ({ location }) => {
   
   useEffect(() => {
     console.log("Counting users...");
-    fetch(`http://127.0.0.1:5000/service-level-api/${location}`)
+    fetch(`${apiURL}/service-level-api/${location}`)
     .then(res => res.json())
     .then(json => setPopularity(json))
   }, [location])
 
 
-  console.log(`http://127.0.0.1:5000/day_average/${location}`)
+  console.log(`${apiURL}/day_average/${location}`)
   console.log("hdata: ", hdata)
   console.log("Popularity: ", popularity)
 
@@ -142,7 +143,7 @@ const PopularityChart = ({ location }) => {
 
     var service_status;
     if ((popularity.count != null) && (i != -1)) {
-      service_status = <ServiceStatus count={liveData[i]} average_count={data[i]}/>
+      service_status = <ServiceStatus count={liveData[i]} average_count={data[i]} apiURL={apiURL}/>
     } else {
       service_status = ""
     }
