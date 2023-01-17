36c7c3b3b0c9e8038a813530a9084cc18e331c3d
StellahFoxtail <99243770+StellahFoxtail@users.noreply.github.com>
2023-01-17 19:07:26 +0900

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => (
           alt="Logo"
         />
         <Link to="/">
-          <h1 style={{fontFamily: "Helvetica", fontSize: "32px", fontWeight: "bold"}} className="flex text-white text-center text-xl md:text-3xl font-bold">Katsura Main Cafeteria</h1>   
+          <h1 style={{fontFamily: "Helvetica", fontSize: "32px", fontWeight: "bold"}} className="flex text-white text-center text-xl md:text-3xl font-bold">Katsura Campus Wi-Fi Times</h1>   
         </Link>
       </div>
     </div>
