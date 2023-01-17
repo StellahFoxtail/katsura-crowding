e3306707ca173c2d8229f7de6edba7e964902598
StellahFoxtail <99243770+StellahFoxtail@users.noreply.github.com>
2023-01-17 19:41:11 +0900

    Update Notice.js

--- frontend/src/components/Notice.js
+++ frontend/src/components/Notice.js
@@ -10,10 +10,21 @@ const Notice = ({ location }) => {
   var message = null;
 
   if (notice_sensors.includes(location)){
-    message = "Cafeteria Soliel is open for lunch from 11:00AM to 2:00PM"
+    message = "Cafeteria Selene is open for lunch from 11:00AM to 2:00PM"
   } else if (location == "AMPM18-KJ008") {
     message = "C CLUSTER Admnistration Office is open from 9:00AM TO 5:00PM"
-  }
+  } else if (location == "AMPM18-KJ009") {
+    message = "C1 Bldng. Entrance is open from 6:00AM and can be acessed with the University Student card after 6:00PM"
+  } else if (location == "AMPM18-KJ011") {
+    message = "B Cluster Admnistration Office is Open from 9:00AM to 5:00PM"
+  } else if (location == "AMPM18-KJ012") {
+    message = "Katsura Mani Student Library is open from 9:00AM to 9:00PM"
+  } else if (location == "AMPM18-KJ013") {
+    message = "P3P Parking at A Cluster Between Buildings A3 and A4 is open 24 Hours"
+  } else if (location == "AMPM18-KJ014") {
+    message = "A4 Rear Entrance is a 24HourOpen short route Access from Katsura Innovation Park Mae Bus Stop at A Cluster"
+  } else if (location == "AMPM18-KJ015") {
+    message = "Boulangerie Seri Bakery is open from 8:00AM to 3:00PM from Monday to Saturday"
 
   console.log("Message: ", message)
   if (message != null) {
