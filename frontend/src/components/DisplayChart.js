0853e638680b242f3cacf139295e66c17187089f
StellahFoxtail <99243770+StellahFoxtail@users.noreply.github.com>
2023-01-17 19:49:03 +0900

    Update DisplayChart.js

--- frontend/src/components/DisplayChart.js
+++ frontend/src/components/DisplayChart.js
@@ -13,12 +13,12 @@ function MyDropdown() {
     <div>
       <select value={selectedOption} onChange={handleChange}>
         <option value="AMPM18-KJ008">C Cluster Admnistration Office</option>
-        <option value="AMPM18-KJ009">C1 Entrance</option>
+        <option value="AMPM18-KJ009">C1 Bldng Entrance</option>
         <option value="AMPM18-KJ011">B Cluster Administration Office</option>
         <option value="AMPM18-KJ012">Katsura Main Student Library</option>
-        <option value="AMPM18-KJ013">A1 Entrance</option>
-        <option value="AMPM18-KJ014">A2 Park Mae</option>
-        <option value="AMPM18-KJ015">Near Bakery</option>
+        <option value="AMPM18-KJ013">A4 Rear Entrance</option>
+        <option value="AMPM18-KJ014">P3P Parking Mae</option>
+        <option value="AMPM18-KJ015">Boulangerie Seri Bakery</option>
         <option value="AMPM18-KJ010">Inside Main Cafeteria</option>
         <option value="AMPM18-KJ016">Main Cafeteria Entrance</option>
         <option value="AMPM18-KJ017">Main Cafeteria Exit</option>
