63e1c1a59366eb67c9cbcd0710e4be8956055780
StellahFoxtail <99243770+StellahFoxtail@users.noreply.github.com>
2023-02-06 00:27:23 +0900

    Update ServiceStatus.js
    Changed service status percentages
    translated service status to Japanese

--- frontend/src/components/ServiceStatus.js
+++ frontend/src/components/ServiceStatus.js
@@ -8,19 +8,19 @@ function ServiceStatus({count, average_count, apiURL, location}) {
   var percent_diff = (count - average_count)/average_count
 
   if (Math.abs(percent_diff) <= 0.05){
-    message = "As busy as usual."
+    message = "いつものように忙しい。"
   }
-  else if (percent_diff > 0.05 && percent_diff <= 0.15) {
-    message = "Busier than usual."
+  else if (percent_diff > 0.05 && percent_diff <= 0.50) {
+    message = "いつもより忙しい。"
   }
-  else if (percent_diff > 0.15) {
-    message = "Much busier than usual."
+  else if (percent_diff > 0.50) {
+    message = "いつもよりずっと忙しい。"
   }
-  else if (percent_diff < -0.05 && percent_diff >= -0.15){
-    message = "Less busier than usual."
+  else if (percent_diff < -0.05 && percent_diff >= -0.50){
+    message = "いつもより忙しくない。"
   }
-  else if (percent_diff < -0.15) {
-    message = "Much less busier than usual."
+  else if (percent_diff < -0.50) {
+    message = "いつもよりずっと忙しくない。"
   } else {
     message = " "
   }
